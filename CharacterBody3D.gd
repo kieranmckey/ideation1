b3c4b1ae7579240d3b55e25cb0c28af4098d3bc1
Kieran Mckey <km319063@falmouth.ac.uk>
2024-06-28 23:06:25 +0100

    corrected sizings

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,9 +1,9 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const MOUSE_SENSITIVITY = 0.02  # turning speed
+const SPEED = 4#4.0
+const JUMP_VELOCITY = 11
+const MOUSE_SENSITIVITY = 0.002  # turning speed
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
